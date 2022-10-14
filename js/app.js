b5eb94d74a44d048403647208386f488bd91e77d
Aymanoli <aymanoli777@gmail.com>
2022-10-14 10:41:25 +0600

    temperature

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const API_KEY = `c896f29585de6d1a13ec0940224a76fd`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature?.weather[0]?.main);
+    // console.log(temperature);
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgicon = document.getElementById('weather-icon');
+    imgicon.setAttribute('src', url);
+}
